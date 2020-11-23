fd18fc7a5477a8edd83f5420a4b3a06b021b0fe1
hatrongvu13 <hatrongvu13@gmail.com>
2020-11-23 17:17:50 +0700

    run.java commit

--- src/Run.java
+++ src/Run.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
 
 public class Run {
+    public static void main(String[] args) {
+        // bai 1
+        Bai1 ex1 = new Bai1();
+        Bai2 ex2 = new Bai2();
+        Bai3 ex3 = new Bai3();
+        ex3.bai3();
+//        System.out.println(ex1.bai1(33));
+//        // bai 2
+//        int[] list = {5,33,11,22,44,55,66};
+//        ex2.bai2(list);
+    }
 }
